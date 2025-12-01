ee6af7780d9a8244f674bdd3898e73b8907f884b
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2025-12-01 08:32:30 -0500

    Excelente buen trabajo
    Gracias por compartirlo

---
+++ src/main/java/com/aputs/Main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Excelente, buen trabajo, att: el profe");
+    }
+}
